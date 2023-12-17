4ab580964bf426def3385743b2745ee3b2ff6182
musfirbaig <musfirbaig27june@gmail.com>
2023-12-17 13:51:15 +0500

    added userUse  useContext, so that userId (obtained from firebase) will be throught available inside children components

---
+++ app/user/UserContext.tsx
@@ -0,0 +1,45 @@
+// UserContext
+"use client"
+import { createContext, useContext, useState } from 'react';
+
+
+import {onAuthStateChanged } from "firebase/auth";
+import {auth} from "../../config/firebase"
+
+// Create a context with an initial value (empty object)
+const UserContext = createContext({});
+
+// Export a provider component
+export const UserProvider = ({ children }:{
+    children: React.ReactNode
+}) => {
+  const [userId, setUserId] = useState("");
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+
+      setUserId(user.uid);
+      // ...
+    } else {
+      // User is signed out
+      // ...
+
+      setUserId("");
+
+    }
+  });
+
+  // Pass the state and methods down through the context provider
+  const contextValue = {
+    userId
+  };
+
+  return <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>;
+};
+
+// Custom hook for consuming the context
+export const useUser = () => {
+  return useContext(UserContext);
+};
